4acfbb1004fb4f93f5ada25c17da858c8311db92
Matteo <me@mattbit.com>
2020-05-17 00:45:29 +0200

    Add REPL banner :sparkles:

--- main.go
+++ main.go
@@ -7,12 +7,21 @@ import (
 	"os/user"
 )
 
+// BANNER is the Monkey opening banner.
+const BANNER = `   __  ___          __
+  /  |/  /__  ___  / /_____ __ __
+ / /|_/ / _ \/ _ \/  '_/ -_) // /
+/_/  /_/\___/_//_/_/\_\\__/\_, /
+                          /___/
+
+`
+
 func main() {
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf(BANNER)
 	fmt.Printf("Hello %s! Welcome to the Monkey REPL.\n", user.Username)
-	fmt.Printf("Type some Mokey code.\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
